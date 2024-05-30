242cd4b5dbb680bbcddc6822de34f8855f92ecac
Ahmad Chauhdry <47418076+0xchauhdry@users.noreply.github.com>
2024-05-30 21:19:35 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   mixpanelKey: '',
-  baseAPIURL: 'http://localhost:3000',
-  logApiUrl: 'http://localhost:3000/log',
+  baseAPIURL: 'https://psnode.vercel.app',
+  logApiUrl: 'https://psnode.vercel.app/log',
   production: false,
   applicationIdentifier: '8es-0XtO8WqT',
   novuAPIKey: 'd816844b2d10ce0b0f7a443978d0692c',
