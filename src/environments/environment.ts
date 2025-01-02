af2a653d05c75e4b7be6cc5492a8ffb1f62bcbb6
Ahmad Chauhdry <47418076+0xchauhdry@users.noreply.github.com>
2025-01-03 00:22:34 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   mixpanelKey: '541f04aa3c17f953c1251f503b382167',
-  baseAPIURL: 'https://api.splitpay.app/api',
+  baseAPIURL: 'https://splitpay.azurewebsites.net/api',
   production: true,
   applicationIdentifier: '8es-0XtO8WqT',
   novuAPIKey: 'd816844b2d10ce0b0f7a443978d0692c',
