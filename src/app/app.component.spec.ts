b7e7ba45608363d5f1c66e9469918e4fa84471a9
Ahmad Chauhdry <47418076+0xchauhdry@users.noreply.github.com>
2025-02-14 02:22:09 +0500

    Chauhdry/101 (#8)
    * group expense filters
    
    * small improvements

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PSClient'`, () => {
+  it(`should have as title 'SplitPay'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PSClient');
+    expect(app.title).toEqual('SplitPay');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PSClient app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('SplitPay app is running!');
   });
 });
